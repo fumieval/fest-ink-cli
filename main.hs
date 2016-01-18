592a1f70a7458e3427b509647eff32be05599823
Fumiaki Kinoshita <fumiexcel@gmail.com>
2016-01-18 18:38:21 +0900

    0.2.0.0

--- main.hs
+++ main.hs
@@ -1,59 +1,104 @@
-{-# LANGUAGE LambdaCase, MultiWayIf, OverloadedStrings #-}
-import qualified Data.Conduit as C
-import Network.HTTP.Conduit
+{-# LANGUAGE LambdaCase, MultiWayIf, OverloadedStrings, DeriveTraversable, Rank2Types, ViewPatterns #-}
+import Control.Applicative
+import Control.Monad.Catch
+import Control.Lens
+import Control.Monad
 import Control.Monad.Trans.Resource (runResourceT)
 import Data.Aeson
 import Data.Aeson.Lens
-import Control.Lens
-import Data.Text.Lens
+import qualified Data.ByteString.Char8 as BC
+import Data.Char
 import Data.Conduit.Attoparsec
-import System.Environment
-import Data.Ord
-import Data.Foldable (for_)
 import Data.Monoid
+import Data.Text (Text)
+import Data.Text.Lens
+import Network.HTTP.Conduit
+import Network.HTTP.Types
+import qualified Data.Conduit as C
+import System.Environment
 import Text.Printf
-import Data.Time.Clock
-import Data.Time.Clock.POSIX
 
-main = do
-  getArgs >>= \case
-    ["index"] -> do  
-      val <- fetch "https://fest.ink/index.json"
-      t <- getPOSIXTime
-      forOf_ (key "fests" . _Array . traverse) val $ \f -> do
-        let i = f ^?! key "id" . _Integer
-        putStr $ if
-          | f ^?! key "term" . key "in_session" . _Bool -> "[" ++ show i ++ "]"
-          | t < fromIntegral (f ^?! key "term" . key "begin" . _Integer) -> "<" ++ show i ++ ">"
-          | otherwise -> " " ++ show i ++ " "
-        putStr "\t"
-        putStrLn $ f ^. key "name" . _String . unpacked
+data SquidSisters a = SquidSisters !a !a deriving (Functor)
 
-    [i] -> do
-      val <- fetch $ "https://fest.ink/" ++ i ++ ".json"
-      let getName cls = val ^?! key "teams" . key cls . key "name" . _String . unpacked
-          nameA = getName "alpha"
-          nameH = getName "bravo"
-          victories cls v = Sum $ v ^?! key cls . _Integer
-          (Sum va, Sum vh) = foldMapOf (key "wins" . _Array . traverse)
-              ((,) <$> victories "alpha" <*> victories "bravo") val
-          vs = fromIntegral (va + vh)
+instance Monoid a => Monoid (SquidSisters a) where
+  mempty = pure mempty
+  mappend = liftA2 mappend
 
-      putStrPadding nameA
-      printf "%.02f\n" (fromIntegral va / vs :: Float)
+instance Applicative SquidSisters where
+  pure a = SquidSisters a a
+  SquidSisters f g <*> SquidSisters a b = SquidSisters (f a) (g b)
 
-      putStrPadding nameH
-      printf "%.02f\n" (fromIntegral vh / vs :: Float)
-    _ -> putStrLn "Usage: fest-ink index | fest-ink <ordinal>"
+alphaBravo :: Applicative f => (Text -> f a) -> f (SquidSisters a)
+alphaBravo f = SquidSisters <$> f "alpha" <*> f "bravo"
 
-  where
-    putStrPadding s = do
-      putStr s
-      putStr $ replicate (16 - length s * 2) ' '
+evalResult :: Value -> Maybe (SquidSisters Double)
+evalResult val = do
+  [votes, win] <- forM ["vote", "win"] $ \ty -> alphaBravo $ \k -> val ^? key ty . key k . _Double
+  mult <- val ^? key "win" . key "multiply" . _Double
+  return $ (+) <$> votes <*> fmap (mult*) win
 
-    fetch url = do
-      man <- newManager tlsManagerSettings
-      req <- parseUrl url
-      runResourceT $ do
-        response <- http req man
-        responseBody response C.$$+- sinkParser json
+strWidth :: String -> Int
+strWidth = sumOf $ traverse . to (\ch -> if isLatin1 ch then 1 else 2)
+
+teamName :: Text -> Traversal' Value String
+teamName k = key "teams" . key k . key "name" . _String . unpacked
+
+fetchResults :: Integer -> IO (Value, SquidSisters Double)
+fetchResults i = do
+  val <- fetch $ "https://fest.ink/" ++ show i ++ ".json"
+  let victories v cls = [v ^. key cls . _Double . to Sum]
+  return $ (,) val $ fmap getSum $ val ^. key "wins" . _Array . traverse . folding (alphaBravo . victories)
+
+fetch :: String -> IO Value
+fetch url = do
+  man <- newManager tlsManagerSettings
+  req <- parseUrl url
+  runResourceT $ do
+    resp <- http req man `catch` \case
+      StatusCodeException (Status i msg) _ _ -> fail $ show i ++ " " ++ BC.unpack msg
+      e -> throwM e
+    responseBody resp C.$$+- sinkParser json
+
+main :: IO ()
+main = getArgs >>= \case
+  [str] | Just i <- str ^? _Show -> do
+    (val, SquidSisters a b) <- fetchResults i
+    putStr $ val ^. key "name" . _String . unpacked
+    case val ^?! key "term" . key "status". _String of
+      "closed" -> putStrLn " (Concluded)"
+      "in session" -> putStrLn " (Open)"
+      "scheduled" -> putStrLn " (Upcoming)"
+      _ -> error "Unknown status"
+    forMOf_ (key "term") val $ \term -> do
+      printf "From: %s\n" $ term ^. key "begin_s" . _String
+      printf "To:   %s\n" $ term ^. key "end_s" . _String
+    forM_ [("alpha", a), ("bravo", b)] $ \(k, n) -> do
+      printf "%s %.0f (%.0f%%)\n" (val ^. teamName k) n (100 * n / (a + b))
+  [] -> do
+    val <- fetch "https://fest.ink/index.json"
+    forOf_ (key "fests" . _Array . traverse) val $ \f -> do
+      let i = f ^?! key "id" . _Integer
+
+      putStr $ show i ++ replicate (4 - length (show i)) ' '
+
+      let result = f ^? key "result" . nonNull . folding evalResult
+
+      status <- case f ^?! key "term" . key "status". _String of
+        "closed" -> case result of
+            Just (SquidSisters a b) -> return $ case compare a b of
+              LT -> "(Winner: " ++ f ^. teamName "bravo" ++ ")"
+              EQ -> "(Even)"
+              GT -> "(Winner: " ++ f ^. teamName "alpha" ++ ")"
+            Nothing -> error "No winner?"
+        "in session" -> do
+            (_, SquidSisters a b) <- fetchResults i
+            return $ printf "(%.2f | %.2f)" (a / (a + b)) (b / (a + b))
+        "scheduled" -> return "(Upcoming)"
+        _ -> error "Unknown status"
+
+      putStr status
+
+      putStr $ replicate (32 - strWidth status) ' '
+
+      putStrLn $ f ^. key "name" . _String . unpacked
+  _ -> putStrLn "Usage: fest-ink [ordinal]"
